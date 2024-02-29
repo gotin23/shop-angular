49b04cbec535eb113e8cf391da6e41ca83477167
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2024-02-29 13:19:06 +0100

    feat: data and assets added

---
+++ src/assets/data/data.ts
@@ -0,0 +1,85 @@
+const data = [
+  {
+    id: 1,
+    name: 'Poste Radio',
+    price: 75,
+    description:
+      'Emportez votre musique partout où vous allez avec notre poste radio portable ! Compact et léger, ce compagnon de voyage vous offre une expérience sonore exceptionnelle, que ce soit à la maison, en plein air ou en déplacement. Profitez de votre station préférée ou branchez votre propre musique pour des moments de détente inégalés !',
+    image: '/assets/images/radio.png',
+  },
+
+  {
+    id: 2,
+    name: 'Gameboy',
+    price: 40,
+    description:
+      "Découvrez le charme rétro de la Gameboy, une icône intemporelle des jeux vidéo ! Plongez dans une expérience de jeu nostalgique avec ce classique portable de Nintendo. Avec sa conception compacte et son écran monochrome, la Gameboy offre des heures de divertissement où que vous soyez. Que vous soyez un collectionneur passionné ou un amateur de jeux vintage, cette console emblématique est un incontournable pour tout fan de jeux rétro. Ne manquez pas l'occasion de posséder cette relique du passé qui a façonné toute une génération de joueurs !",
+    image: '/assets/images/gameboy.png',
+  },
+  {
+    id: 3,
+    name: 'Minitel',
+    price: 90,
+    description:
+      "Redécouvrez le charme rétro de la technologie avec notre Minitel ! Ce petit terminal vous ramène dans les années 80 avec son design emblématique et ses fonctionnalités simples mais efficaces. Envoyez des messages, consultez les services en ligne et plongez dans la nostalgie de l'ère numérique naissante, le tout depuis le confort de votre foyer !",
+    image: '/assets/images/minitel.png',
+  },
+  {
+    id: 4,
+    name: 'Nokia 3310',
+    price: 22,
+    description:
+      "Découvrez le charme rétro de la Gameboy, une icône intemporelle des jeux vidéo ! Plongez dans une expérience de jeu nostalgique avec ce classique portable de Nintendo. Avec sa conception compacte et son écran monochrome, la Gameboy offre des heures de divertissement où que vous soyez. Que vous soyez un collectionneur passionné ou un amateur de jeux vintage, cette console emblématique est un incontournable pour tout fan de jeux rétro. Ne manquez pas l'occasion de posséder cette relique du passé qui a façonné toute une génération de joueurs !",
+    image: '/assets/images/nokia.png',
+  },
+  {
+    id: 5,
+    name: 'Walkman',
+    price: 35,
+    description:
+      "Revivez la nostalgie de la musique portable avec notre Walkman classique ! Compact, léger et doté d'un son cristallin, cet appareil emblématique vous permet d'emporter votre musique préférée partout où vous allez. Glissez votre cassette préférée, mettez vos écouteurs et plongez dans une expérience sonore immersive comme dans les années 80 !",
+    image: '/assets/images/walkman.png',
+  },
+  {
+    id: 6,
+    name: 'Platine vinyle',
+    price: 120,
+    description:
+      "Retrouvez le son authentique et chaleureux du vinyle avec notre platine vinyle haut de gamme ! Offrant une reproduction sonore exceptionnelle et un design élégant, cette platine est l'accessoire ultime pour les audiophiles et les amateurs de musique. Redécouvrez vos vinyles préférés avec une clarté et une profondeur incomparables, créant ainsi une expérience d'écoute immersive et inoubliable.",
+    image: '/assets/images/bigben.png',
+  },
+
+  {
+    id: 7,
+    name: 'Tamagotchi',
+    price: 24,
+    description:
+      "Offrez-vous un compagnon de jeu unique avec nos Tamagotchis ! Ces petites créatures numériques sont prêtes à vous suivre partout où vous allez, offrant des heures de divertissement et de lien affectif. Avec une variété de modèles disponibles, il y en a pour tous les goûts. Adoptez le vôtre aujourd'hui et lancez-vous dans une aventure virtuelle sans fin !",
+    image: '/assets/images/tamagotchi.png',
+  },
+  {
+    id: 8,
+    name: 'Lecteur vhs',
+    price: 42,
+    description:
+      "Redécouvrez vos souvenirs avec notre lecteur VHS ! Plongez dans le passé et revivez vos moments préférés capturés sur bande avec une clarté surprenante. Notre lecteur VHS offre une qualité d'image nette et une compatibilité avec une large gamme de cassettes, vous permettant de revivre vos précieux souvenirs avec une simplicité moderne.",
+    image: '/assets/images/vhs.png',
+  },
+  {
+    id: 9,
+    name: 'Furbys',
+    price: 18,
+    description:
+      "Plongez dans le monde magique des Furbys avec notre collection exclusive ! Ces adorables créatures interactives sont prêtes à devenir vos compagnons de jeu préférés. Dotés de personnalités uniques et de capacités d'apprentissage, les Furbys offrent des heures de divertissement pour les enfants de tous âges. Adoptez le vôtre aujourd'hui et laissez la magie commencer !",
+    image: '/assets/images/furbys.png',
+  },
+  {
+    id: 10,
+    name: 'Téléviseur',
+    price: 20,
+    description:
+      "Découvrez le charme intemporel d'un téléviseur vintage ! Avec son design rétro et son écran cathodique classique, notre téléviseur offre une touche de nostalgie à votre espace de vie. Plongez dans les souvenirs du passé tout en appréciant vos émissions préférées avec ce véritable morceau d'histoire de la technologie. Ajoutez une touche de caractère à votre décor avec ce téléviseur vintage qui évoque une époque révolue.",
+    image: '/assets/images/televiseur.png',
+  },
+];
+export default data;
