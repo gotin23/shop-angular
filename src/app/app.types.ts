e06b15cac089c1186817f001dc1ecf5d645ebb90
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2024-02-29 14:01:59 +0100

    feat: typescript types added

---
+++ src/app/app.types.ts
@@ -0,0 +1,14 @@
+export type Article = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+};
+export type CartItem = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+};
