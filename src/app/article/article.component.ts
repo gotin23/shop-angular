7560ffa596d38a1c74bce6093f312d9aab585723
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2024-02-29 14:06:56 +0100

    feat: article card and modal component added

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../services/cart.service';
+import type { Article } from '../app.types';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [ModalComponent, CommonModule],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css',
+})
+export class ArticleComponent {
+  @Input() article!: Article;
+  showModal: boolean = false;
+  displayModal: () => void = () => {
+    console.log('showModal');
+    this.showModal = true;
+  };
+  constructor(private cartService: CartService) {}
+  addToCart(article: Article) {
+    this.cartService.increaseCartItem(article);
+  }
+}
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Article, CartItem } from '../app.types';
+import { CartService } from '../services/cart.service';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  @Input() article!: Article;
+
+  @Input() visible!: boolean;
+  @Output() visibleChange = new EventEmitter<boolean>();
+  constructor(private cartService: CartService) {}
+  items: Array<CartItem> = [];
+
+  addToCart(article: Article) {
+    this.cartService.increaseCartItem(article);
+  }
+}
